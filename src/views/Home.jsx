e9200280d9fb6a9ac0705bddad375b273f8f114d
gonzalofz <102331798+gonzalofz@users.noreply.github.com>
2022-10-07 22:19:55 -0300

    first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => (
+  <div className="title">
+    <h1>Bienvenido Maestro Pokemón</h1>
+    <img
+      src="https://media.tenor.com/JufMsoOI750AAAAC/pikachu-pokemon.gif"
+      alt=""
+    />
+  </div>
+);
+
+export default Home;
